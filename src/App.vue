20fea28f1815e53cc3d4ca304c2cfa7ed0305fc0
eugeneChu <hiendat.chu@gmail.com>
2024-06-25 14:54:57 +0700

    add animation and finish with vertically (v) collapse/expand

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex flex-col h-screen">
+    <Topbar />
+    <div class="flex flex-1">
+      <Sidebar />
+      <Content />
+    </div>
+    <Footer />
+  </div>
 </template>
+
+<script>
+import Topbar from './components/Topbar/Topbar.vue'
+import Footer from './components/Footer/Footer.vue'
+import Sidebar from './components/Sidebar/Sidebar.vue'
+import Content from './components/Content/Content.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Topbar,
+    Footer,
+    Sidebar,
+    Content
+  }
+}
+</script>
+
+<style>
+/* Add global styles here if needed */
+</style>
--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -5,104 +5,106 @@
         <h2 class="text-gray-900 dark:text-black font-bold text-lg">GA4</h2>
         <img :src="ga4Icon" alt="Toggle" class="h-5 w-5" />
       </div>
-      <ul v-if="isGa4Expanded">
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">Overview</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/ga4/detail_traffic.svg"
-            alt="Detail Traffic"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Detail Traffic</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/ga4/traffic_acquisition.svg"
-            alt="Traffic Acquisition"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Traffic Acquisition</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/ga4/engagements.svg"
-            alt="Engagements"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Engagements</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/ga4/detail_event.svg"
-            alt="Detail Event"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Detail Event</span>
-        </li>
-      </ul>
+      <transition name="dropdown">
+        <ul v-show="isGa4Expanded" class="overflow-hidden">
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Overview</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/ga4/detail_traffic.svg"
+              alt="Detail Traffic"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Detail Traffic</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/ga4/traffic_acquisition.svg"
+              alt="Traffic Acquisition"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Traffic Acquisition</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/ga4/engagements.svg"
+              alt="Engagements"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Engagements</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/ga4/detail_event.svg"
+              alt="Detail Event"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Detail Event</span>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div class="mb-6">
       <div class="flex items-center justify-between cursor-pointer" @click="toggleGroup('gsc')">
         <h2 class="text-gray-900 dark:text-black font-bold text-lg">GSC</h2>
         <img :src="gscIcon" alt="Toggle" class="h-5 w-5" />
       </div>
-      <ul v-if="isGscExpanded">
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">Overview</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/gsc/detail.svg" alt="Detail" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">Detail</span>
-        </li>
-      </ul>
+      <transition name="dropdown">
+        <ul v-show="isGscExpanded" class="overflow-hidden">
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Overview</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/gsc/detail.svg" alt="Detail" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Detail</span>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div>
       <div class="flex items-center justify-between cursor-pointer" @click="toggleGroup('order')">
         <h2 class="text-gray-900 dark:text-black font-bold text-lg">Order</h2>
         <img :src="orderIcon" alt="Toggle" class="h-5 w-5" />
       </div>
-      <ul v-if="isOrderExpanded">
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">Overview</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/order/seo.svg" alt="SEO" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">SEO</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/order/google_ads.svg"
-            alt="Google Ads"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Google Ads</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img src="@/images/sidebar_icons/order/direct.svg" alt="Direct" class="h-5 w-5 mr-2" />
-          <span class="text-gray-700 dark:text-black">Direct</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/order/facebook.svg"
-            alt="Facebook"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Facebook</span>
-        </li>
-        <li class="flex items-center py-2">
-          <img
-            src="@/images/sidebar_icons/order/data_manager.svg"
-            alt="Data Manager"
-            class="h-5 w-5 mr-2"
-          />
-          <span class="text-gray-700 dark:text-black">Data Manager</span>
-        </li>
-      </ul>
+      <transition name="dropdown">
+        <ul v-show="isOrderExpanded" class="overflow-hidden">
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/overview.svg" alt="Overview" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Overview</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/order/seo.svg" alt="SEO" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">SEO</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/order/google_ads.svg"
+              alt="Google Ads"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Google Ads</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/order/direct.svg" alt="Direct" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Direct</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img src="@/images/sidebar_icons/order/facebook.svg" alt="Facebook" class="h-5 w-5 mr-2" />
+            <span class="text-gray-700 dark:text-black">Facebook</span>
+          </li>
+          <li class="flex items-center py-2">
+            <img
+              src="@/images/sidebar_icons/order/data_manager.svg"
+              alt="Data Manager"
+              class="h-5 w-5 mr-2"
+            />
+            <span class="text-gray-700 dark:text-black">Data Manager</span>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div class="flex justify-center mt-6">
       <button class="bg-lightblue dark:bg-darkblue text-white p-2 rounded-full">
@@ -113,9 +115,9 @@
 </template>
 
 <script>
-import angleUp from '@/images/sidebar_icons/angle-up.svg'
-import angleDown from '@/images/sidebar_icons/angle-down.svg'
-import collapseIcon from '@/images/sidebar_icons/collapse.svg'
+import angleUp from '@/images/sidebar_icons/angle-up.svg';
+import angleDown from '@/images/sidebar_icons/angle-down.svg';
+import collapseIcon from '@/images/sidebar_icons/collapse.svg';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -129,16 +131,16 @@ export default {
   },
   computed: {
     ga4Icon() {
-      return this.isGa4Expanded ? angleDown : angleUp
+      return this.isGa4Expanded ? angleDown : angleUp;
     },
     gscIcon() {
-      return this.isGscExpanded ? angleDown : angleUp
+      return this.isGscExpanded ? angleDown : angleUp;
     },
     orderIcon() {
-      return this.isOrderExpanded ? angleDown : angleUp
+      return this.isOrderExpanded ? angleDown : angleUp;
     },
     collapseIcon() {
-      return collapseIcon
+      return collapseIcon;
     }
   },
   methods: {
@@ -147,10 +149,18 @@ export default {
         !this[`is${group.charAt(0).toUpperCase() + group.slice(1)}Expanded`]
     },
     imageLoaded() {
-      console.log('Image loaded successfully')
+      console.log('Image loaded successfully');
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.dropdown-enter-active, .dropdown-leave-active {
+  transition: all 0.5s ease;
+}
+.dropdown-enter, .dropdown-leave-to {
+  height: 0;
+  opacity: 0;
+}
+</style>
