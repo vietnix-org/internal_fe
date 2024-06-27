c3c867ce33eae3fcd64db278c6224f28a860c8f8
eugeneChu <hiendat.chu@gmail.com>
2024-06-27 14:51:32 +0700

    init topbar

--- src/components/Topbar/Topbar.vue
+++ src/components/Topbar/Topbar.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="bg-blue-500 text-white p-4">
-      Topbar
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Topbar'
-  }
-  </script>
-  
-  <style scoped>
-  /* Add your styles here if needed */
-  </style>
-  
+  <div class="bg-topbar-gradient h-16">
+    just the top bar
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topbar",
+};
+</script>
+
+<style scoped>
+/* Any additional styling for the topbar can go here */
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         darkblue: '#5885AF',  // Dark blue
         sidebarDark: '#68BBE3', 
       },
+      backgroundImage: {
+        'topbar-gradient': 'linear-gradient(46deg, #A3BDED 0%, #6991C7 100%)',
+      },
     },
   },
   plugins: [],
